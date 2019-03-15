7ada3ab3f058d9cb7d2670d03d5c0597820f1b00
Kafca <kafca@qq.com>
2019-03-15 16:54:41 +0800

    多核并行处理音频识别' '

--- audio_to_words.py
+++ audio_to_words.py
@@ -1,32 +1,59 @@
 from aip import AipSpeech
+from datetime import datetime
 import os
-
-""" 你的 APPID AK SK """
-APP_ID = '15657858'
-API_KEY = 'hpfsSY5PnhEBSZk3mjbgoSTH'
-SECRET_KEY = 'lceEAytImCR61u6ZpmigtgiZN9smU0xg'
-
-client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+import multiprocessing
 
 
 # 读取文件
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
+def get_file_content(file_path):
+    with open(file_path, 'rb') as fp:
         return fp.read()
 
 
-# 打开文件
-path = "./speech-vad-demo/output_pcm"
-dirs = os.listdir(path)
+class AudioToWords:
+    APP_ID = '15657858'
+    API_KEY = 'hpfsSY5PnhEBSZk3mjbgoSTH'
+    SECRET_KEY = 'lceEAytImCR61u6ZpmigtgiZN9smU0xg'
 
-results_text = []
+    client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
-# 识别本地文件
-for file in dirs:
-    data = client.asr(get_file_content('./speech-vad-demo/output_pcm/'+file), 'pcm', 16000, {
-        'dev_pid': 1536,
-    })
-    if data["err_no"] == 0:
-        results_text.append(data["result"][0])
+    # 文件路径
+    path = "./speech-vad-demo/output_pcm/"
+    dirs = os.listdir(path)
 
+    # 识别结果
+    results_text = []
+
+    def audio_to_words(self, filename):
+        data = self.client.asr(get_file_content(self.path + filename), 'pcm', 16000, {
+            'dev_pid': 1536,
+        })
+        if data["err_no"] == 0:
+            self.results_text.append(data["result"][0])
+            return data["result"][0]
+        else:
+            return str(data["err_no"])
+
+
+start_time = datetime.now()
+
+cores = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=cores)
+
+# 多核并行处理，音频识别
+worker = AudioToWords()
+results_text = pool.map(worker.audio_to_words, worker.dirs)
+
+print("dirs:"+str(len(worker.dirs)))
+print("results_text:"+str(len(results_text)))
 print(results_text)
+
+print("cores:", cores)
+end_time = datetime.now()
+print("Time used:", end_time-start_time)
+
+# 识别结果写入text.txt
+with open('text.txt', 'wt', encoding='utf-8') as f:
+    for text in results_text:
+        f.write(text)
+        f.write(",")
